a2329f48a288d3cf42d4bcf1b65791f3ac3aabdf
Vaibhav Gaur <VaibhavGaur56789@gmail.com>
2021-04-09 14:05:21 +0530

    Add files via upload

---
+++ 3_implementation/inc/writeScore.h
@@ -0,0 +1,22 @@
+//#ifndef writeScore
+
+void writescore(float score, char plnm[20])
+{
+	float sc;
+	char nm[20];
+	FILE *f;
+	system("cls");
+	f=fopen("score.txt","r");
+	fscanf(f,"%s%f",&nm,&sc);
+	if (score>=sc)
+	{
+		sc=score;
+		fclose(f);
+		f=fopen("score.txt","w");
+		fprintf(f,"%s\n%.2f",plnm,sc);
+		fclose(f);
+	}
+}
+
+//#define writeScore
+//#endif // writeScore
